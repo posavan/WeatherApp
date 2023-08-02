1b5474839e888fcf17bf915f7101e049964da29b
niko po <posavan.okin@gmail.com>
2023-08-02 13:18:00 -0400

    added React and basic API functionality

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,6 +1,21 @@
 import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude is: ", lat);
+    console.log("Longitude is: ", long);
+  }, [lat, long]);
+
   return (
     <div className="App">
 
